b83afa886ca5eb137f424eb64779074bf5394823
Marina Santiago <marinasantiago1718@gmail.com>
2025-09-29 15:17:04 -0300

    Adição de git

---
+++ src/main.js
@@ -0,0 +1,50 @@
+const artistInput = document.getElementById("artistInput");
+const searchButton = document.getElementById("searchButton");
+const results = document.getElementById("results");
+
+
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+
+async function searchArtist() {
+  const artistName = artistInput.value.trim();
+  if(!artistName) {
+    results.innerHTML = '<p>Please enter an artist name.</p>';
+    return;
+  }
+
+  try {
+    
+    const response = await fetch(`${proxyUrl}https://theaudiodb.com/api/v1/json/2/search.php?s=${encodeURIComponent(artistName)}`);
+    
+    if(!response.ok) {
+      throw new Error("Erro de acesso à API");
+    }
+
+    const json = await response.json();
+
+    if(!json.artists) {
+      results.innerHTML = '<p>No artists found.</p>';
+      return;
+    }
+    console.log(json);
+    results.innerHTML = ""; 
+
+    for (let i = 0; i < 3 && i < json.artists.length; i++) {
+      const artist = json.artists[i];
+
+        results.innerHTML += `
+          <div class="artistCard">
+          <h2>${artist.strArtist}</h2>
+          <img src="${artist.strArtistThumb }" alt="${artist.strArtist}" />
+          <p><a href="${artist.strLastFMChart}" target="_blank">Ver no Last.fm</a></p>
+          </div>
+        `;
+    }
+  }
+
+   catch (error) {
+    results.innerHTML = `<p>Erro: Problema de busca</p>`;
+  }
+}
+
+searchButton.addEventListener("click", searchArtist);
